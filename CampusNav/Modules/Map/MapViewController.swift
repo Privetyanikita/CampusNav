81f7edfd187247250a0b9c5d2789cef3bfbd01f4
Nikita Kornuk <kornyk62@gmail.com>
2026-03-19 09:33:13 +0300

    add new images

--- CampusNav/Modules/Map/MapViewController.swift
+++ CampusNav/Modules/Map/MapViewController.swift
@@ -17,10 +17,10 @@ final class MapViewController: UIViewController {
 
     private let buildings: [Building] = [
         // latitude/longitude координаты геопозиции
-        Building(title: "Факультет экономики, управления и права \n Корпус 1", subtitle: "Перейти", latitude: 47.101420, longitude: 37.525576),
-        Building(title: "Факультет филологии и массовых коммуникаций \n Корпус 2", subtitle: "Перейти", latitude: 47.102119, longitude: 37.525756),
-        Building(title: "Педагогический факультет \n Корпус 3-4", subtitle: "Перейти", latitude: 47.109443, longitude: 37.530238),
-        Building(title: "Факультет гуманитарных и социальных наук \n Корпус 5", subtitle: "Перейти", latitude: 47.099420, longitude: 37.536302),
+        Building(title: "Факультет экономики, управления и права \n Корпус 1", subtitle: "Перейти", latitude: 47.101420, longitude: 37.525576, imageName: "firstCampus"),
+        Building(title: "Факультет филологии и массовых коммуникаций \n Корпус 2", subtitle: "Перейти", latitude: 47.102119, longitude: 37.525756, imageName: "secondCampus"),
+        Building(title: "Педагогический факультет \n Корпус 3-4", subtitle: "Перейти", latitude: 47.109443, longitude: 37.530238, imageName: nil),
+        Building(title: "Факультет гуманитарных и социальных наук \n Корпус 5", subtitle: "Перейти", latitude: 47.099420, longitude: 37.536302, imageName: nil),
     ]
 
     // MARK: - UI
@@ -61,47 +61,6 @@ final class MapViewController: UIViewController {
             let card = makeBuildingCard(building: building, tag: index)
             stackView.addArrangedSubview(card)
         }
-    }
-
-    // MARK: - Card Factory
-
-    private func makeBuildingCard(building: Building, tag: Int) -> UIView {
-        let container = UIView()
-        container.backgroundColor = .secondarySystemGroupedBackground
-        container.layer.cornerRadius = 12
-        container.layer.borderWidth = 0.5
-        container.layer.borderColor = UIColor.separator.cgColor
-        container.tag = tag
-
-        let titleLabel = UILabel()
-        titleLabel.text = building.title
-        titleLabel.font = .systemFont(ofSize: 16, weight: .semibold)
-        titleLabel.textColor = .label
-        titleLabel.numberOfLines = 0
-        titleLabel.textAlignment = .center
-
-        let subtitleLabel = UILabel()
-        subtitleLabel.text = building.subtitle
-        subtitleLabel.font = .systemFont(ofSize: 14, weight: .regular)
-        subtitleLabel.textColor = .systemBlue
-        subtitleLabel.textAlignment = .center
-
-        let innerStack = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
-        innerStack.axis = .vertical
-        innerStack.spacing = 4
-        innerStack.alignment = .center
-        innerStack.isUserInteractionEnabled = false
-
-        container.addSubview(innerStack)
-
-        innerStack.snp.makeConstraints { make in
-            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 14, left: 16, bottom: 14, right: 16))
-        }
-
-        let tap = UITapGestureRecognizer(target: self, action: #selector(buildingCardTapped(_:)))
-        container.addGestureRecognizer(tap)
-
-        return container
     }
 
     // MARK: - Actions
@@ -155,5 +114,63 @@ extension MapViewController {
             make.edges.equalToSuperview().inset(UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16))
             make.width.equalToSuperview().offset(-32)
         }
+    }
+    
+    private func makeBuildingCard(building: Building, tag: Int) -> UIView {
+        let container = UIView()
+        container.backgroundColor = .secondarySystemGroupedBackground
+        container.layer.cornerRadius = 12
+        container.layer.borderWidth = 0.5
+        container.layer.borderColor = UIColor.separator.cgColor
+        container.tag = tag
+
+        let titleLabel = UILabel()
+        titleLabel.text = building.title
+        titleLabel.font = .systemFont(ofSize: 16, weight: .semibold)
+        titleLabel.textColor = .label
+        titleLabel.numberOfLines = 0
+        titleLabel.textAlignment = .center
+
+        let subtitleLabel = UILabel()
+        subtitleLabel.text = building.subtitle
+        subtitleLabel.font = .systemFont(ofSize: 14, weight: .regular)
+        subtitleLabel.textColor = .systemBlue
+        subtitleLabel.textAlignment = .center
+
+        let textStack = UIStackView(arrangedSubviews: [titleLabel, subtitleLabel])
+        textStack.axis = .vertical
+        textStack.spacing = 4
+        textStack.alignment = .center
+        textStack.isUserInteractionEnabled = false
+        
+        let mainStack = UIStackView()
+        mainStack.axis = .vertical
+        mainStack.spacing = 12
+        mainStack.alignment = .fill
+        mainStack.isUserInteractionEnabled = false
+        
+        if let imageName = building.imageName, let image = UIImage(named: imageName) {
+            let imageView = UIImageView(image: image)
+            imageView.contentMode = .scaleAspectFill
+            imageView.clipsToBounds = true
+            imageView.layer.cornerRadius = 8
+            imageView.snp.makeConstraints { make in
+                make.height.equalTo(140)
+            }
+            mainStack.addArrangedSubview(imageView)
+        }
+        
+        mainStack.addArrangedSubview(textStack)
+
+        container.addSubview(mainStack)
+
+        mainStack.snp.makeConstraints { make in
+            make.edges.equalToSuperview().inset(UIEdgeInsets(top: 14, left: 16, bottom: 14, right: 16))
+        }
+
+        let tap = UITapGestureRecognizer(target: self, action: #selector(buildingCardTapped(_:)))
+        container.addGestureRecognizer(tap)
+
+        return container
     }
 }
---
+++ CampusNav/Supporting files/Image/Images.swift
@@ -0,0 +1,21 @@
+//
+//  Images.swift
+//  CampusNav
+//
+//  Created by Nikita on 19.03.2026.
+//
+
+import UIKit
+
+enum Images {
+    enum NewsCell {
+        static let newsImage: UIImage = .april
+        static let newsTwoImage: UIImage = .march
+        static let newsThreeImage: UIImage = .konkurs
+        static let newsFourImage: UIImage = .theOpenDay
+    }
+    enum CampusPhotoCell {
+        static let firstImage: UIImage = .firstCampus
+        static let secondImage: UIImage = .secondCampus
+    }
+}
